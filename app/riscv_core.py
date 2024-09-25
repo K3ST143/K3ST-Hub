9740a3c8bdef562e14df73bddb3f7752204d738f
K3ST <smr_wl@yahoo.com>
2024-09-26 02:08:45 +0300

    Create riscv_core.py

---
+++ app/riscv_core.py
@@ -0,0 +1,12 @@
+class RISCVCore:
+    def __init__(self):
+        self.registers = [0] * 32
+        self.pc = 0
+
+    def execute_instruction(self, instruction):
+        # Placeholder for instruction execution logic
+        pass
+
+    def load_program(self, program):
+        # Placeholder for loading program into memory
+        pass
